9b18d270a77af55611d0ef26bbe5a47f61adf314
aperher <aperher@inf.upv.es>
2023-09-28 13:52:37 +0200

    WeatherViewModel.kt ready

--- app/src/main/java/com/plcoding/weatherapp/presentation/WeatherState.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/WeatherState.kt
@@ -3,5 +3,7 @@ package com.plcoding.weatherapp.presentation
 import com.plcoding.weatherapp.domain.weather.WeatherInfo
 
 data class WeatherState(
-    val weatherInfo: WeatherInfo? = null
+    val weatherInfo: WeatherInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
 )
