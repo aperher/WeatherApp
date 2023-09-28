18a55194a994ce7578727cdd6241b3986b6f664b
aperher <aperher@inf.upv.es>
2023-09-28 14:34:47 +0200

    WeatherCard.kt UI almost completed

---
+++ app/src/main/java/com/plcoding/weatherapp/presentation/WeatherCard.kt
@@ -0,0 +1,92 @@
+package com.plcoding.weatherapp.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.plcoding.weatherapp.domain.weather.WeatherData
+import com.plcoding.weatherapp.domain.weather.WeatherInfo
+import com.plcoding.weatherapp.domain.weather.WeatherType
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@Composable
+fun WeatherCard(
+    state: WeatherState,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    state.weatherInfo?.currentWeatherData?.let { data ->
+        Card(
+            backgroundColor = backgroundColor,
+            shape = RoundedCornerShape(10.dp),
+            modifier = modifier.padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Today ${data.time.format(DateTimeFormatter.ofPattern("HH:mm"))}",
+                    modifier = Modifier.align(Alignment.End),
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Image(
+                    painter = painterResource(id = data.weatherType.iconRes),
+                    contentDescription = null,
+                    modifier = Modifier.width(200.dp)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "${data.temperatureCelsius}°C",
+                    fontSize = 50.sp,
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = data.weatherType.weatherDesc,
+                    fontSize = 20.sp,
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(name = "Weather Card", showSystemUi = true, showBackground = true)
+fun WeatherCardPreview() {
+    WeatherCard(
+        state = WeatherState(
+            weatherInfo = WeatherInfo(
+                weatherDataPerDay = mapOf(),
+                currentWeatherData = WeatherData(
+                    time = LocalDateTime.now(),
+                    temperatureCelsius = 20.0,
+                    pressure = 20.0,
+                    windSpeed = 20.0,
+                    humidity = 20.0,
+                    weatherType = WeatherType.ClearSky
+                )
+            )
+        ),
+        backgroundColor = Color(0xFF1E1E1E)
+    )
+}
